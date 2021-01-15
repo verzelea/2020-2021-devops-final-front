6c44265dfd7f2bb6cd2bc2f3657dd41eb3cb0a17
verzelea <arthurverzele@hotmail.com>
2021-01-15 17:44:02 +0100

    :green_heart: fix cache key

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://tobedefined.io/api"
+    ? process.env.PROD_BACK_URL || "URL_HERE"
     : "http://localhost:2020/api";
 export default API_URL;
