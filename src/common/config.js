dbdd912c73c53add4b8ac4fb3b13b3920dc3b333
verzelea <arthurverzele@hotmail.com>
2021-01-17 15:24:46 +0100

    :wrench: change url in settings

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? process.env.PROD_BACK_URL || "https://devopstpfinal-front.herokuapp.com/api"
+    ? "https://devoptpfinal.herokuapp.com/"
     : "http://localhost:2020/api";
 export default API_URL;
